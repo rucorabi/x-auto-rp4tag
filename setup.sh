945ba2fda2997e45004ac3c355ac09adcea2e7c3
rucorabi <rucorabi+github@gmail.com>
2024-10-14 09:25:59 +0900

    setupスクリプトにバリデーション追加

--- setup.sh
+++ setup.sh
@@ -5,4 +5,10 @@ rm .clasp.json
 # 引数からスプレッドシートIDを取得
 SPREADSHEET_ID=$1
 
+# IDが見つからない場合はエラー
+if [ -z ${SPREADSHEET_ID} ]; then
+  echo "スプレッドシートIDが指定されていません"
+  exit 1
+fi
+
 npx clasp create --parentId=${SPREADSHEET_ID}
